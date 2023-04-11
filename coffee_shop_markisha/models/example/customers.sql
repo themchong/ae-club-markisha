0d7c9db241a4ca0a31a24f9d422d581cdb993ae3
Markisha Chong <79109569+themchong@users.noreply.github.com>
2023-04-11 23:26:05 +0000

    installs dbt

---
+++ coffee_shop_markisha/models/example/customers.sql
@@ -0,0 +1,10 @@
+SELECT orders.customer_id
+, customers.name
+, customers.email 
+, MIN(orders.created_at) AS first_order_at
+, COUNT(distinct orders.id) AS number_of_orders
+FROM analytics-engineers-club.coffee_shop.customers
+LEFT JOIN
+analytics-engineers-club.coffee_shop.orders ON customers.id = orders.customer_id
+GROUP BY orders.customer_id, customers.name, customers.email
+ORDER BY first_order_at LIMIT 5;
